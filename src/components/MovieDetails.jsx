f9d986bc5f24562397efd7ca30b6756e3c9a9090
Raja <118312686+yorkzap@users.noreply.github.com>
2024-04-10 05:54:27 -0700

    Include streaming options in Movie Details

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import CastList from './CastList'; // Import the CastList component
-import './MovieDetails.css'; // Import CSS for MovieDetails component
+import CastList from './CastList';
+import StreamingAvailability from './StreamingAvailability';
+import './MovieDetails.css';
 
 function MovieDetails({ movieData }) {
   if (!movieData) {
@@ -8,32 +9,33 @@ function MovieDetails({ movieData }) {
   }
 
   return (
-    <div className="cast-listt">
+    <div className="movie-details-wrapper">
+      <div className="movie-details-container">
+        <div className="poster-container">
+          <div className="poster-background" />
+          {movieData.poster_path && (
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}
+              alt={`Poster for ${movieData.title}`}
+              className="poster-image"
+            />
+          )}
+        </div>
+        <div className="details">
+          <h2 className="title">{movieData.title}</h2>
+          <p><strong>Release Date:</strong> {movieData.release_date}</p>
+          <p><strong>Overview:</strong> {movieData.overview}</p>
+          <p><strong>Vote Average:</strong> {movieData.vote_average}</p>
+        </div>
+      </div>
+      {/* Streaming Availability */}
+      <StreamingAvailability movieId={movieData.id} />
+      {/* Cast List */}
       {movieData.cast && movieData.cast.length > 0 && (
-          <div className="cast-list-container"> {/* Wrap the CastList with a container */}
-            <div className="cast-list">
-              <CastList cast={movieData.cast} /> {/* Pass cast data to the CastList component */}
-            </div>
-          </div>
-        )}
-    <div className="movie-details-container">
-      <div className="poster-container">
-        <div className="poster-background" />
-        {movieData.poster_path && (
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movieData.poster_path}`}
-            alt={`Poster for ${movieData.title}`}
-            className="poster-image"
-          />
-        )}
-      </div>
-      <div className="details">
-        <h2 className="title">{movieData.title}</h2>
-        <p><strong>Release Date:</strong> {movieData.release_date}</p>
-        <p><strong>Overview:</strong> {movieData.overview}</p>
-        <p><strong>Vote Average:</strong> {movieData.vote_average}</p>
-      </div>
-    </div>
+        <div className="cast-list-container">
+          <CastList cast={movieData.cast} />
+        </div>
+      )}
     </div>
   );
 }
